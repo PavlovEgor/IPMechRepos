b76f797991967c5cb9b927bc308ed918b1ef0401
PavlovEgor <pavlov.ep@phystech.edu>
2025-04-14 15:32:09 +0300

    Add comments

--- UPostProc/main.py
+++ UPostProc/main.py
@@ -8,7 +8,6 @@ import pandas as pd
 До начала выполнения программы необходимо в рабочей папке openfoam выполнить команду
 postProcess -func writeCellCentres
 
-
 Входные данные: 
 path - адресс папки с расчетом openfoam. 
 time - время нужного кейса
@@ -17,29 +16,37 @@ InputData - датафрейм с эксель таблицей заданных
 Выходные данные: 
 OutputData - датафрейм с эксель таблицей искомых скоростей.
 
+Предостережения:
+1) Программа не выдает ошибки, если заданная точка находится за пределами тела. 
+2) 
 """
 path = '/home/tgd323/OpenFOAM/tgd323-v2406/run/6Step/3D/angleSearch/a-10/BlockMesh/h-35/l-50/100CH4-0H2-air-6-step3D/'
 time = '0.00400258'
 
-U = Ofpp.parse_internal_field(path + time + '/U')
-Cx = Ofpp.parse_internal_field(path + time + '/Cx')
-Cy = Ofpp.parse_internal_field(path + time + '/Cy')
-Cz = Ofpp.parse_internal_field(path + time + '/Cz')
+U = Ofpp.parse_internal_field(path + time + '/U') # скорость массив из векторов [Ux, Uy, Uz] | shape = (n, 3)
+Cx = Ofpp.parse_internal_field(path + time + '/Cx')  # x-координата элементов сетки | shape = (n, 1)
+Cy = Ofpp.parse_internal_field(path + time + '/Cy')  # y-координата элементов сетки
+Cz = Ofpp.parse_internal_field(path + time + '/Cz')  # z-координата элементов сетки
 
 
-InputData = pd.read_excel("input.xlsx")
+InputData = pd.read_excel("input.xlsx")  # массив заданных точек | shape = (num_of_points, 3)
 num_of_points = InputData.shape[0]
 InputData = InputData.T
-OutputData= pd.DataFrame({"X": np.zeros(num_of_points), "Y": np.zeros(num_of_points), "Z": np.zeros(num_of_points),
-                          "Ux": np.zeros(num_of_points), "Uy": np.zeros(num_of_points), "Uz": np.zeros(num_of_points),
-                          "Umag": np.zeros(num_of_points)})
+
+OutputData= pd.DataFrame({"X": np.zeros(num_of_points),
+                          "Y": np.zeros(num_of_points),
+                          "Z": np.zeros(num_of_points),
+                          "Ux": np.zeros(num_of_points),
+                          "Uy": np.zeros(num_of_points),
+                          "Uz": np.zeros(num_of_points),
+                          "Umag": np.zeros(num_of_points)})  # | shape = (num_of_points, 7)
 
 
 for point in range(num_of_points):
-    r0 = InputData[point].array
+    r0 = InputData[point].array  # заданная точка
 
-    residual = (Cx - r0[0]) ** 2 + (Cy - r0[1]) ** 2 + (Cz - r0[2]) ** 2
-    ind = np.argmin(residual)
+    residual = (Cx - r0[0]) ** 2 + (Cy - r0[1]) ** 2 + (Cz - r0[2]) ** 2  # массив расстояний до заданной точки
+    ind = np.argmin(residual)  # индекс ячейки, от которой расстояние до заданной минимально
 
     OutputData.loc[point, "X"] = Cx[ind]
     OutputData.loc[point, "Y"] = Cy[ind]
